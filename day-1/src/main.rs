3b0604f769d8dc36dfdd3124896ddbfb7af478df
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-12-01 12:45:06 +0300

    Day 1 Part One

---
+++ day-1/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("./input.txt").expect("Unable to read file");
+    let numbers_int: Vec<u16> = data.lines().map(|e| e.parse::<u16>().unwrap()).collect();
+    let mut counter = 0;
+    let mut prev = numbers_int[0];
+    for i in 1..numbers_int.len() {
+        if numbers_int[i] > prev {
+            counter += 1;
+        }
+        prev = numbers_int[i];
+    }
+    println!("{}", counter);
+}
